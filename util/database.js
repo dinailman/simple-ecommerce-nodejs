82a9c37e64c784d45303cd0e0ec10dd111b0c13c
dinailman <dinailman@ymail.com>
2020-12-22 13:50:57 +0700

    chore(env) : [dotenv] env file and usage

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
 const {Sequelize} = require("sequelize");
+const env = require("dotenv").config();
 
-const sequelize = new Sequelize('simple-ecommerce', 'root', '', {
-    dialect: 'mysql',
-    host: 'localhost'
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    timezone: '+07:00'
 });
 
 module.exports = sequelize;
